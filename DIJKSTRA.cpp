dae774a39f70f38d17489c2209974ebbc252d748
suman60 <smmandol60@gmail.com>
2023-12-17 17:23:30 +0600

    Add header file

--- DIJKSTRA.cpp
+++ DIJKSTRA.cpp
@@ -1,3 +1,6 @@
+#include <bits/stdc++.h>
+using namespace std;
+#define ll long long 
 class dijkstra {
 public:
     dijkstra(int n) : n(n), adj(n) {}
--- DSU.cpp
+++ DSU.cpp
@@ -1,3 +1,5 @@
+#include <bits/stdc++.h>
+using namespace std;
 class DSU {
 public:
     DSU(int n) {
